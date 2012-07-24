9c854413d3b430cb739d4cde64396e24b36fa7be
Helen Dong <imobile2@gmail.com>
2012-07-24 13:55:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0256ddb04393477115fddd751d317cc0edc7316f11df1900cfb72d1ad404861960261d09b4df33632fda9c81b670bcae479a5ef125aad8ff2acf05295281e14b'
