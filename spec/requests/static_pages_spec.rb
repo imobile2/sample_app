32a0258aa5f65f8020e22def218a3f2f1eec2b8e
Helen Dong <imobile2@gmail.com>
2012-07-25 11:35:34 -0700

    pretty tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 describe "StaticPages" do
    subject {page}
+   
+   shared_examples_for "all static pages" do
+     it { should have_selector('h1',    text: heading) }
+     it { should have_selector('title', text: full_title(page_title)) }
+   end
+   
    describe "Home page" do
-      before { visit root_path }
-      
-      it { should have_selector('h1', text: 'Sample App') }
-      it { should have_selector 'title',
-                          text: "Ruby on Rails Tutorial Sample App" }
-      it { should_not have_selector 'title', text: '| Home' }
-    end
+     before { visit root_path }
+     let(:heading)    { 'Sample App' }
+     let(:page_title) { '' }
+
+     it_should_behave_like "all static pages"
+     it { should_not have_selector 'title', text: '| Home' }
+   end
+   
     describe "Help page" do
       before { visit help_path }
 
@@ -27,5 +34,20 @@ describe "StaticPages" do
 
        it { should have_selector('h1',    text: 'Contact') }
        it { should have_selector('title', text: full_title('Contact')) }
+     end
+     
+     it "should have the right links on the layout" do
+       visit root_path
+       click_link "About"
+       page.should have_selector 'title', text: full_title('About Us')
+       click_link "Help"
+       page.should # fill in
+       click_link "Contact"
+       page.should # fill in
+       click_link "Home"
+       click_link "Sign up now!"
+       page.should # fill in
+       click_link "sample app"
+       page.should # fill in
      end
 end
